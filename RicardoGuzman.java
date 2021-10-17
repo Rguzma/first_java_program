04dba61a28e4506bcd04dbe5a2c355950aed878e
Your Name <you@example.com>
2021-10-17 15:15:57 -0600

    rguzma

---
+++ RicardoGuzman.java
@@ -0,0 +1,7 @@
+public class RicardoGuzman {
+    public static void main(String[] args) {
+        System.out.println("My name is Ricardo Guzman.");
+        System.out.println("I am 30 years old.");
+        System.out.println("I live in Costa Rica.");
+    }
+}
